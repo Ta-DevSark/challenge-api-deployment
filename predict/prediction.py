d2ac0f328d2d74a0e6c98e06ce42a069270690ad
Ta-DevSark <tadevosian.sarkis@gmail.com>
2023-06-06 11:16:24 +0200

    predictions ok

--- predict/prediction.py
+++ predict/prediction.py
@@ -15,14 +15,14 @@ import joblib
 
 house_df = pd.read_csv("final_house.csv")
 
-# Remove outliers
-
 X = house_df
 
 y = house_df['Price']
 
-X =  X.drop(['Unnamed: 0','Zip', 'Price' , 'Locality','id', 'Primary energy consumption','Type of property',
+X =  X.drop(['Heating type','Surroundings type','Unnamed: 0','Zip', 'Price' , 'Locality','id', 'Primary energy consumption','Type of property',
                                          'Subtype of property', 'Energy class', 'Province'], axis=1)
+
+X = X.fillna(0)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=101)
 
